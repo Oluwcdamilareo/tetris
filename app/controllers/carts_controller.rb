1770b613c9af43f5cbe19e61e24d6770ba6ad2fe
Oluwadamilare Olusakin <Oluwadamilare_olusakin@yahoo.com>
2017-03-11 05:28:04 +0100

    Add to cart and display cart

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,22 @@
+class CartsController < ApplicationController
+  def show
+    @order_items = current_order.order_items
+  end
+
+  def update
+    @order_items = current_order.order_items
+    @order_items = current_order.order_items.find(params[:id])
+    @order_items.update_attributes(order_item_params)
+  end
+
+  def destroy
+    @order_items = current_order.order_items
+    @order_items = current_order.order_items.find(params[:id])
+    @order_items.destroy
+  end
+
+  def create
+    @order_items = current_order.order_items.new(order_item_params)
+    @order_items.save
+  end
+end
