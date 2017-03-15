b5756fe779b119e2a2682ff433f05aae20dd5f73
Oluwadamilare Olusakin <Oluwadamilare_olusakin@yahoo.com>
2017-03-15 17:43:37 +0100

    Add admin dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
 
 
+
+  # get  '/admins/tetrisadsignin00231', to: 'admin/sessions#new'
+  # post '/admins/sign_in', to: 'devise/sessions#create'
+  # delete '/admins/sign_out', to: 'devise/sessions#destroy'
+  # post  '/admins/password', to:  'devise/passwords#create'
+  # get   '/admins/password/new', to: 'devise/passwords#new'
+  # get    '/admins/password/edit', to: 'devise/passwords#edit'
+  # patch '/admins/password',      to:'devise/passwords#update'
+  # put   '/admins/password', to: 'devise/passwords#update'
+  # get   '/admins/cancel', to:    'devise/registrations#cancel'
+  # post   '/admins', to: 'devise/registrations#create'
+  # # get  '/admins/sign_up', to: 'devise/registrations#new', as:'tetrisadsignin00232'
+  # get  '/admins/edit', to: 'devise/registrations#edit'
+  # patch  '/admins', to: 'devise/registrations#update'
+  # put '/admins', to: 'devise/registrations#update'
+  # delete '/admins', to: 'devise/registrations#destroy'
+  #
+  #
+  #
+  #
+
   devise_for :admins
+  
   get 'orders/show'
 
   get 'orders/create'
@@ -31,6 +53,6 @@ Rails.application.routes.draw do
 
   get '/account', to: 'static_pages#account'
 
-  get '/admin', to: 'admins#admin_account', as: 'addacc'
+  get '/dashboard', to: 'static_pages#admin_dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
